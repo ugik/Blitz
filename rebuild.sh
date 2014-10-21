975ac375c747bbae04b4c94afbffacbbdddefc2e
ugik <georgek@gmail.com>
2014-10-21 14:26:13 -0400

    trainer no-blitz scenario

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,16 @@
+rm blitz/usermedia/headshots/*.jp*
+rm blitz/usermedia/headshots/*.png
+rm blitz/usermedia/checkins/*.jp*
+rm blitz/usermedia/checkins/*.png
+rm blitz/usermedia/documents/*
+rm blitz/usermedia/feed/*.jp*
+rm blitz/usermedia/feed/*.png
+rm blitz/usermedia/logos/*.jp*
+rm blitz/usermedia/logos/*.png
+mv blitz/usermedia/programs/sample_blitz_program.xls blitz/usermedia/programs/sample_blitz_program.KEEP
+rm blitz/usermedia/programs/*.xls
+mv blitz/usermedia/programs/sample_blitz_program.KEEP blitz/usermedia/programs/sample_blitz_program.xls
+
 python manage.py syncdb --noinput
 python manage.py migrate --all
 python manage.py setup_test_env --traceback
