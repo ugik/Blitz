e4402674a7a6d67d4c08fea7bd16c3c8649c8084
ugik <georgek@gmail.com>
2014-10-20 21:17:40 -0400

    send_mail silent=False if DEBUG

--- base/emails.py
+++ base/emails.py
@@ -13,7 +13,10 @@ SOURCE_EMAIL = 'team@blitz.us'
 SPOTTER_EMAIL = 'spotters@blitz.us'
 
 # email wrapper, note parameters: images[] context{}
-def send_email(from_email, to_email, subject, text_template, html_template, context, images=[], dirs=[], override=None, silent=True):  
+def send_email(from_email, to_email, subject, text_template, html_template, context, images=[], dirs=[], override=None):  
+
+    if settings.DEBUG:
+        silent = False
 
     if len(images) == 0:
         images = ['emailheader.png']
