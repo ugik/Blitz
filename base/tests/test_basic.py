4dada8c09eba5c952821a788ddd61e937466eea4
ugik <georgek@gmail.com>
2014-10-25 20:36:09 -0400

    ssl setup

--- base/tests/test_basic.py
+++ base/tests/test_basic.py
@@ -10,7 +10,6 @@ import unittest, time, re
 class TestBasic(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.set_window_size(800, 1000)
         self.driver.implicitly_wait(30)
         self.base_url = "http://127.0.0.1:8000"
         self.verificationErrors = []
@@ -19,7 +18,6 @@ class TestBasic(unittest.TestCase):
     def test_basic(self):
         driver = self.driver
         driver.get(self.base_url + "/login?standard")
-        time.sleep(2)
         driver.find_element_by_id("id_email").clear()
         driver.find_element_by_id("id_email").send_keys("mr@example.com")
         driver.find_element_by_id("id_password").clear()
@@ -32,17 +30,15 @@ class TestBasic(unittest.TestCase):
         driver.find_element_by_css_selector("button.obtn.obtn-comment").click()
         # Warning: assertTextPresent may require manual changes
         self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*$")
-        driver.find_element_by_css_selector("img").click()
-        time.sleep(2)
+        driver.find_element_by_css_selector("a.btn.btn-navbar").click()
         # Warning: assertTextPresent may require manual changes
         self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*$")
         driver.find_element_by_css_selector("b.caret").click()
         driver.find_element_by_css_selector("ul.dropdown-menu > li > a").click()
         driver.find_element_by_css_selector("a.btn.btn-navbar").click()
-        driver.find_element_by_link_text("Inbox").click()
+        driver.find_element_by_xpath("//a[contains(@href, '/inbox')]").click()
         driver.find_element_by_css_selector("a.btn.btn-navbar").click()
         driver.find_element_by_link_text("Pages").click()
-        time.sleep(2)
         driver.find_element_by_css_selector("button.obtn.obtn-comment-grn").click()
         driver.back()
         driver.find_element_by_link_text("http://127.0.0.1:8000/Mike/3weeks").click()
@@ -51,40 +47,34 @@ class TestBasic(unittest.TestCase):
         driver.back()
         driver.find_element_by_link_text("http://127.0.0.1:8000/Mike/3weeks/signup").click()
         driver.back()
-        time.sleep(2)
         driver.find_element_by_css_selector("button.obtn.full-width").click()
         # Warning: assertTextPresent may require manual changes
         self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*$")
         driver.find_element_by_css_selector("button.obtn.obtn-comment-half").click()
         driver.find_element_by_id("why").clear()
         driver.find_element_by_id("why").send_keys("this is the why work with me section")
-        driver.find_element_by_css_selector("form[name=\"SalesBlitz\"] > div.submit-container > button.obtn.obtn-comment-half").click()
-        # Warning: assertTextPresent may require manual changes
-        self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*$")
+        driver.find_element_by_css_selector("#primary_logo > img").click()
         driver.back()
         driver.back()
         driver.back()
         driver.back()
         driver.find_element_by_css_selector("a.btn.btn-navbar").click()
         driver.find_element_by_link_text("Programs").click()
-        time.sleep(2)
         driver.find_element_by_css_selector("button.obtn.obtn-comment-grn").click()
         driver.back()
         driver.find_element_by_css_selector("a.btn.btn-navbar").click()
         driver.find_element_by_link_text("Dashboard").click()
-        driver.find_element_by_css_selector("li.solo-client > div.item > img.img-circle.headshot").click()
-        driver.find_element_by_xpath("//div[@id='dashboard']/div/ul/li[4]/div/img").click()
         driver.find_element_by_css_selector("img").click()
-        driver.find_element_by_css_selector("img.basic-tooltip.img-circle").click()
+        driver.find_element_by_link_text("Luke Walton").click()
         driver.find_element_by_link_text("Progress").click()
         driver.find_element_by_link_text("Check-ins").click()
         driver.find_element_by_link_text("History").click()
-        driver.find_element_by_css_selector("img").click()
-        time.sleep(2)
+        driver.find_element_by_css_selector("a.btn.btn-navbar").click()
+        driver.find_element_by_link_text("Home").click()
         driver.find_element_by_xpath("//img[@alt='Tayshawn Prince']").click()
         driver.find_element_by_css_selector("img").click()
         driver.find_element_by_css_selector("a.btn.btn-navbar").click()
-        driver.get(self.base_url + "")
+        driver.get(self.base_url + "/logout")
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
