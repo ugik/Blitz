93fa33f538307b96c6f653c6c9d934b3b6797b12
ugik <georgek@gmail.com>
2015-02-05 08:52:18 -0500

    django hijack

--- blitz/urls.py
+++ blitz/urls.py
@@ -23,6 +23,7 @@ urlpatterns += patterns(
     '',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hijack/', include('hijack.urls')),
 
     url(r'^allclients$', 'base.views.all_clients', name='all_clients'),
 
