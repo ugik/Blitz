3f3b6f324e8a8b5474ccffdb2c6fef2245b563cf
ugik <georgek@gmail.com>
2015-02-09 20:53:39 -0500

    spotter workoutplan copy

--- spotter/urls.py
+++ spotter/urls.py
@@ -26,6 +26,7 @@ urlpatterns = patterns(
 
     url(r'^status_trainers$', 'spotter.views.spotter_status_trainers', name='spotter_status_trainers'),
     url(r'^assign_workoutplan$', 'spotter.views.assign_workoutplan', name='spotter_assign_workoutplan'),
+    url(r'^copy_workoutplan$', 'spotter.views.copy_workoutplan', name='spotter_copy_workoutplan'),
 
     url(r'^download$', 'spotter.views.spotter_download', name='spotter_download'),
     url(r'^delete$', 'spotter.views.spotter_delete', name='spotter_delete'),
