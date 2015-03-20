8892b974f24916fdac49ab9c4a823727e3e83b27
ugik <georgek@gmail.com>
2015-03-20 09:22:39 -0400

    /all columns alternating

---
+++ base/templatetags/get_prior_col.py
@@ -0,0 +1,13 @@
+from django import template
+register = template.Library()
+
+def get_col(value, counter):
+  return value[counter]
+
+def get_prior_col(value, counter):
+  if counter>0:
+      return value[counter-1]
+  else:
+      return value[counter]
+
+register.filter('get_col', get_col)
